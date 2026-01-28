3d6ad70f9c7536b8227c509750e76b117f4772c5
JulesBS <bertron.simpson@gmail.com>
2026-01-28 11:15:48 +0900

    Initial Commit

---
+++ EgoFixTests/EgoFixTests.swift
@@ -0,0 +1,17 @@
+//
+//  EgoFixTests.swift
+//  EgoFixTests
+//
+//  Created by Jules Bertron-Simpson on 28/01/2026.
+//
+
+import Testing
+@testable import EgoFix
+
+struct EgoFixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
